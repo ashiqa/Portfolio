3ac2748c96a4482cbe8082590d8db3f353b99c85
ashiqa <rahmanashiqa@gmail.com>
2020-10-29 20:47:52 +0530

    initial

--- js/main.js
+++ js/main.js
@@ -19,12 +19,6 @@ $(document).ready(function () {
 
     $('.project-area .button-group #btn1').trigger('click');
 
-    $('.project-area .grid .test-popup-link').magnificPopup({
-        type: 'image',
-        gallery: { enabled: true }
-    });
-
-
     // Owl-carousel
 
     $('.site-main .about-area .owl-carousel').owlCarousel({
